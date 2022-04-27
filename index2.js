cea6fc1a9c39c7ca099c30063b3c5e3e218d8889
Diego Mateus <alejandromateus31@outlook.com>
2022-04-27 18:53:30 -0500

    Terminado

---
+++ index2.js
@@ -0,0 +1,119 @@
+const templateCard = document.getElementById('template-carrito').content
+const cards = document.getElementById('cards')
+const items = document.getElementById('items')
+const fragment = document.createDocumentFragment()
+var input = document.getElementById("myInput");
+let AuxJsonData = [];
+let contador = 0;
+let contadorR = 0;
+let bandera = true;
+let numerosRandom =[];
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+     console.log('es una de las pruebas');
+    fetchData()
+    
+});
+
+
+const fetchData = async() => {
+
+    try {
+        const res = await fetch('api.json')
+        const data = await res.json()
+        AuxJsonData = data;     
+        controlaCards();
+        contadorR = AuxJsonData.length - 1;
+    } catch (error) {
+
+    }
+}
+
+
+
+
+
+const pintarcards = (item) => {
+
+    items.innerHTML = '';
+
+        templateCard.querySelector('h1').textContent = AuxJsonData[item].id       
+        const clone = templateCard.cloneNode(true)       
+        fragment.appendChild(clone)
+        items.appendChild(fragment)
+        agregarInput(item)
+        document.querySelector("#myInput").focus();
+
+    
+}
+
+
+const controlaCards = () =>{
+
+
+        console.log(AuxJsonData.length - 1);
+        if(contador <= AuxJsonData.length - 1)
+        {            
+            console.log("entro " + contador) ;
+            pintarcards(contador);   
+            contador ++;       
+                  
+        }else{
+            if(0 < contadorR){
+                contadorR --;       
+                pintarcards(contadorR);   
+
+            }else{
+
+              
+
+                    if(numerosRandom.length != AuxJsonData.length ){
+                        let numeroRamdon ;
+                        while(true){
+                            numeroRamdon = randomIntFromInterval(0 , AuxJsonData.length  - 1 );
+                            if(! numerosRandom.includes(numeroRamdon)){
+                                numerosRandom.push(numeroRamdon);
+                               break;
+                            }
+                        }
+
+                        pintarcards(numeroRamdon);
+                       
+                    }else{
+                        console.log("Termino......");
+                    }
+            }
+        }
+}
+
+
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+
+
+const agregarInput = (item) =>{
+    //debugger;
+    document.querySelector('#myInput').addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+          
+          if((document.querySelector('#myInput').value) == AuxJsonData[item].title){
+             
+          };
+
+         
+            controlaCards();
+                  
+           
+         
+        }     
+
+    });
+
+}
+
+
